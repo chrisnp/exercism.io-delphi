25312c2495eea0ad58357bf08ca0b5a6654aa9f6
Invigo <invigo.devel@gmail.com>
2018-08-26 17:15:11 +0300

    hamming & updated .gitignore

---
+++ hamming/uHamming.pas
@@ -0,0 +1,27 @@
+unit uHamming;
+
+interface
+
+  type
+    THamming = class
+      public
+        class function Distance(strand1, strand2 : String) : integer;
+        static;
+    end;
+
+implementation
+  uses System.SysUtils;
+
+  class function THamming.Distance(strand1, strand2 : String) : integer;
+  var
+    i : integer;
+  begin
+    if strand1.Length <> strand2.Length  then
+      raise EArgumentException.Create('error: left and right strands must be of equal length');
+    result := 0;
+    for i := Low(strand1) to High(strand1) do
+      if strand1[i] <> strand2[i] then
+        result := result + 1;
+  end;
+
+end.
---
+++ hamming/uHammingTests.pas
@@ -0,0 +1,166 @@
+unit uHammingTests;
+
+interface
+uses
+  DUnitX.TestFramework;
+
+const
+  CanonicalVersion = '2.1.0';
+
+type
+
+  [TestFixture]
+  HammingTests = class(TObject)
+  public
+    [Test]
+//  [Ignore('Comment the "// [Ignore]" statement to run the test')]
+    procedure empty_strands;
+
+    [Test]
+    // [Ignore]
+    procedure identical_strands;
+
+    [Test]
+    // [Ignore]
+    procedure long_identical_strands;
+
+    [Test]
+    // [Ignore]
+    procedure complete_distance_in_single_nucleotide_strands;
+
+    [Test]
+    // [Ignore]
+    procedure complete_distance_in_small_strands;
+
+    [Test]
+    // [Ignore]
+    procedure small_distance_in_small_strands;
+
+    [Test]
+    // [Ignore]
+    procedure small_distance;
+
+    [Test]
+    // [Ignore]
+    procedure small_distance_in_long_strands;
+
+    [Test]
+    // [Ignore]
+    procedure non_unique_character_in_first_strand;
+
+    [Test]
+    // [Ignore]
+    procedure non_unique_character_in_second_strand;
+
+    [Test]
+    // [Ignore]
+    procedure same_nucleotides_in_different_positions;
+
+    [Test]
+    // [Ignore]
+    procedure large_distance;
+
+    [Test]
+    // [Ignore]
+    procedure large_distance_in_off_by_one_strand;
+
+    [Test]
+    // [Ignore]
+    procedure disallow_first_strand_longer;
+
+    [Test]
+    // [Ignore]
+    procedure disallow_second_strand_longer;
+  end;
+
+implementation
+uses System.SysUtils, uHamming;
+
+procedure HammingTests.empty_strands;
+begin
+  Assert.AreEqual(0,THamming.Distance('',''));
+end;
+
+procedure HammingTests.identical_strands;
+begin
+  Assert.AreEqual(0,THamming.Distance('A','A'));
+end;
+
+procedure HammingTests.long_identical_strands;
+begin
+  Assert.AreEqual(0, THamming.Distance('GGACTGA', 'GGACTGA'));
+end;
+
+procedure HammingTests.complete_distance_in_single_nucleotide_strands;
+begin
+  Assert.AreEqual(1, THamming.Distance('A', 'G'));
+end;
+
+procedure HammingTests.complete_distance_in_small_strands;
+begin
+  Assert.AreEqual( 2, THamming.Distance('AG', 'CT'));
+end;
+
+procedure HammingTests.small_distance_in_small_strands;
+begin
+  Assert.AreEqual(1, THamming.Distance('AT', 'CT'));
+end;
+
+procedure HammingTests.small_distance;
+begin
+  Assert.AreEqual(1, THamming.Distance('GGACG', 'GGTCG'));
+end;
+
+procedure HammingTests.small_distance_in_long_strands;
+begin
+  Assert.AreEqual(2, THamming.Distance('ACCAGGG', 'ACTATGG'));
+end;
+
+procedure HammingTests.non_unique_character_in_first_strand;
+begin
+  Assert.AreEqual(1, THamming.Distance('AAG', 'AAA'));
+end;
+
+procedure HammingTests.non_unique_character_in_second_strand;
+begin
+  Assert.AreEqual(1, THamming.Distance('AAA', 'AAG'));
+end;
+
+procedure HammingTests.same_nucleotides_in_different_positions;
+begin
+  Assert.AreEqual(2, THamming.Distance('TAG', 'GAT'));
+end;
+
+procedure HammingTests.large_distance;
+begin
+  Assert.AreEqual(4, THamming.Distance('GATACA', 'GCATAA'));
+end;
+
+procedure HammingTests.large_distance_in_off_by_one_strand;
+begin
+  Assert.AreEqual(9, THamming.Distance('GGACGGATTCTG', 'AGGACGGATTCT'));
+end;
+
+procedure HammingTests.disallow_first_strand_longer;
+var MyProc: TTestLocalMethod;
+begin
+  MyProc := procedure
+            begin
+              THamming.Distance('AATG', 'AAA');
+            end;
+  Assert.WillRaiseWithMessage(MyProc, EArgumentException, 'error: left and right strands must be of equal length');
+end;
+
+procedure HammingTests.disallow_second_strand_longer;
+var MyProc: TTestLocalMethod;
+begin
+  MyProc := procedure
+            begin
+              THamming.Distance('ATA', 'AGTG');
+            end;
+  Assert.WillRaiseWithMessage(MyProc, EArgumentException, 'error: left and right strands must be of equal length');
+end;
+
+initialization
+  TDUnitX.RegisterTestFixture(HammingTests);
+end.
