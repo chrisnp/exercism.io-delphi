87b99f68f69cee949628d50d4814bf3d5e4ebca8
Invigo <invigo.devel@gmail.com>
2018-08-31 02:20:45 +0300

    refactored difference-of-squares

--- difference-of-squares/uDifferenceOfSquares.pas
+++ difference-of-squares/uDifferenceOfSquares.pas
@@ -7,33 +7,22 @@ interface
   function differenceOfSquares(maxNum: Integer): Integer;
 
 implementation
-  uses System.Generics.Collections;
 
   function squareOfSum(maxNum: Integer): Integer;
-  var numbers: TList<Integer>;
-      sum, i, n: Integer;
+  var sum, n: Integer;
   begin
-    numbers := TList<integer>.Create;
-    for i := 1 to maxNum do
-      numbers.Add(i);
     sum := 0;
-    for n in numbers do
+    for n := 1 to maxNum do
       sum := sum + n;
     result := sum * sum;
   end;
 
   function sumOfSquares(maxNum: Integer): Integer;
-  var squares: TList<Integer>;
-      i, n: Integer;
+  var i, n: Integer;
   begin
-    squares := TList<Integer>.Create;
-    for i := 1 to maxNum do
-    begin
-      squares.Add(i * i);
-    end;
     result := 0;
-    for n in squares do
-      result := result + n;
+    for n := 1 to maxNum do
+      result := result + (n*n);
   end;
 
   function differenceOfSquares(maxNum: Integer): Integer;
