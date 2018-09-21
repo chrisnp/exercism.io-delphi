0e10dd422fc2cf108af6adf6a0f09dcf3f308a4f
Invigo <invigo.devel@gmail.com>
2018-09-21 05:26:17 +0300

    grade-school refactor

--- grade-school/uGradeSchool.pas
+++ grade-school/uGradeSchool.pas
@@ -2,83 +2,100 @@ unit uGradeSchool;
 
 interface
 
-uses
-  System.Generics.Collections;
+  uses
+    System.Generics.Collections;
 
-type
-  TRoster = TList<String>;
-  ISchool = Interface
-    procedure Add(name : String; grade : Integer);
-    function Roster : TRoster;
-    function Grade(grade : Integer) : TRoster;
-  End;
-  function School : ISchool;
+  type
+    TRoster = TList<String>;
+
+    ISchool = Interface
+      procedure Add(name: String; grade: Integer);
+      function Roster: TRoster;
+      function grade(grade: Integer): TRoster;
+    End;
+
+  function NewSchool: ISchool;
 
 implementation
 
   type
     TSchool = class(TInterfacedObject, ISchool)
     private
-      studentsInGrades : TDictionary<Integer, TRoster>;
+      studentsInGrades: TDictionary<Integer, TRoster>;
     public
       constructor Create;
-//      destructor Destroy;
-      procedure Add(name : String; grade : Integer);
-      function Roster : TRoster;
-      function Grade(grade : Integer) : TRoster;
+      destructor Destroy;
+      procedure Add(name: String; grade: Integer);
+      function Roster: TRoster;
+      function grade(grade: Integer): TRoster;
     end;
 
-  function School : ISchool;
+  constructor TSchool.Create;
+  begin
+    studentsInGrades := TDictionary<Integer, TRoster>.Create;
+  end;
+
+  destructor TSchool.Destroy;
+  var
+    g: TPair<Integer, TRoster>;
+  begin
+    if studentsInGrades.Count > 0 then
+    begin
+      for g in studentsInGrades do
+      begin
+        g.Value.Clear;
+        g.Value.DisposeOf;
+      end;
+      studentsInGrades.DisposeOf;
+    end;
+  end;
+
+  function NewSchool: ISchool;
   begin
     result := TSchool.Create;
   end;
 
-  constructor TSchool.Create;
-  begin
-    studentsInGrades := TDictionary<Integer,TRoster>.Create;
-  end;
-
-  procedure TSchool.Add(name : String; grade : Integer);
+  procedure TSchool.Add(name: String; grade: Integer);
   var
-    new : TRoster;
-  begin
-    if (studentsInGrades.ContainsKey(grade)) then
-      studentsInGrades.Items[grade].Add(name)
-    else
-    begin
-      new := TRoster.Create;
-      studentsInGrades.Add(grade, new);
-      new.Add(name);
-    end;
-  end;
-
-  function TSchool.Grade(grade: Integer) : TRoster;
-  var
-    r : TRoster;
+    someGrade: TRoster;
   begin
     if studentsInGrades.ContainsKey(grade) then
     begin
-      r := studentsInGrades.Items[grade];
-      r.Sort;
-      result := r;
+      someGrade := studentsInGrades.Items[grade];
+      someGrade.Add(name);
+    end
+    else
+    begin
+      someGrade := TRoster.Create;
+      studentsInGrades.Add(grade, someGrade);
+      someGrade.Add(name);
+    end;
+  end;
+
+  function TSchool.grade(grade: Integer): TRoster;
+  var
+    Roster: TRoster;
+  begin
+    if studentsInGrades.ContainsKey(grade) then
+    begin
+      Roster := studentsInGrades.Items[grade];
+      Roster.Sort;
+      result := Roster;
     end
     else
       result := TRoster.Create;
-
   end;
 
   function TSchool.Roster: TRoster;
   var
-    grade: TPair<integer, TRoster>;
-    roster: TRoster;
-    index: integer;
+    i: Integer;
+    arr: TArray<Integer>;
   begin
     result := TRoster.Create;
-    for index := Low to High do
-
-    for index := fLowGrade to fHighGrade do
-      if fRoster.TryGetValue(idxGrade, lRoster) then
-        result.AddRange(lRoster.ToArray);
+    arr := studentsInGrades.Keys.ToArray;
+    TArray.Sort<Integer>(arr);
+    for i in arr do
+      result.AddRange(grade(i).ToArray);
   end;
 
 end.
