91893fef6052972bf64a22bfc37807c38532da36
Invigo <invigo.devel@gmail.com>
2018-09-26 17:48:01 +0300

    refactored collatz-conjecture

--- collatz-conjecture/uCollatzconjecture.pas
+++ collatz-conjecture/uCollatzconjecture.pas
@@ -6,20 +6,18 @@ uses SysUtils;
 type
   EIllegalNumber = class(Exception);
 
-  function collatz(number: integer): integer;
   function collatzSteps(number: integer): integer;
 
 implementation
 
-function collatz(number: integer): integer;
- begin
-  if not odd(number) then
-    result := number div 2    
-  else
-    result := (number * 3) + 1;
-end;
-
 function collatzSteps(number: integer): integer;
+  function collatz(number: integer): integer;
+  begin
+    if not odd(number) then
+      result := number div 2
+    else
+      result := (number * 3) + 1;
+  end;
 begin
   if number <= 0 then
     raise EIllegalNumber.Create('error: Only positive numbers are allowed');
@@ -27,7 +25,7 @@ begin
   while (number > 1) and (result < 1000) do
   begin
     number := collatz(number);
-    result := result + 1;
+    inc(result);
   end;
 end;
 
