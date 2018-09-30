55066cba38171d134aebda44fa96bd6eda7a8ba0
Invigo <invigo.devel@gmail.com>
2018-10-01 00:04:02 +0300

    refactor binary-search

--- binary-search/uBinarySearch.pas
+++ binary-search/uBinarySearch.pas
@@ -13,8 +13,7 @@ implementation
 class function BinarySearch.Search(inputList : TArray<Integer>;
                                    value : Integer ) : Integer;
 
-  function RecSearch(inputList : TArray<Integer>;
-                     val, loIdx, hiIdx : Integer) : Integer;
+  function RecSearch(val, loIdx, hiIdx : Integer) : Integer;
   var
     midx : Integer;
   begin
@@ -26,14 +25,14 @@ class function BinarySearch.Search(inputList : TArray<Integer>;
       if (val = inputList[midx]) then
         result := midx
       else if (val < inputList[midx]) then
-        result := RecSearch(inputList, val, loIdx, midx - 1)
+        result := RecSearch(val, loIdx, midx - 1)
       else
-        result := RecSearch(inputList, val, midx + 1, hiIdx);
+        result := RecSearch(val, midx + 1, hiIdx);
     end;
   end;
 
 begin
-  result := RecSearch(inputList, value, Low(inputList), High(inputList));
+  result := RecSearch(value, Low(inputList), High(inputList));
 end;
 
 end.
---
+++ isbn-verifier/uISBNVerifier.pas
@@ -0,0 +1,7 @@
+unit uISBNVerifier;
+
+interface
+
+implementation
+
+end.
